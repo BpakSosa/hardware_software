69eb8cb7a58dda3ce156843d70afb0c473dde3e6
BpakSosa <1ben0sosa1@gmail.com>
2024-01-08 12:20:08 -0500

    firstday2

---
+++ awesomecode.py
@@ -0,0 +1,13 @@
+def main():
+    print("yeo, wusg, whats ur name")
+    name = input()
+    print()
+    print("aight wassup " + name)
+    print("do u fw programming?")
+    answer = input()
+    print()
+    print('cool, ' + name + ' u said um ' + answer + '?')
+    print("go live ur life or whatevr")
+
+if __name__ == "__main__":
+    main()
